a9d9c8a93fb64c55d52bb6115dbe0969850cfcbc
Brian D <bdugg@keemail.me>
2022-08-17 17:34:39 -0400

    Define DB schema

---
+++ server/db.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE restaurant_list;
+
+CREATE TYPE category AS ENUM (
+  'Chinese',
+  'Diner',
+  'Eastern Other',
+  'Fast Food',
+  'French',
+  'Greek',
+  'Hungarian',
+  'Italian',
+  'Japanese',
+  'Mediterranean'
+  'Mexican',
+  'Pizza',
+  'Pub Grub',
+  'Thai'
+);
+
+CREATE TABLE restaurants(
+  restaurant_id SERIAL PRIMARY KEY,
+  restaurant_name VARCHAR(128) NOT NULL,
+  restaurant_category category NOT NULL,
+  rating SMALLINT CHECK (rating BETWEEN 0 AND 5) NOT NULL,
+  last_visit DATE
+);
